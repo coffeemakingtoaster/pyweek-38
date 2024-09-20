e2b17dc745bd479fac33471025d22b96b16dd579
Freezerino <lk169@hdm-stuttgart.de>
2024-09-20 11:24:20 +0200

    add controls tutorial

--- ui/pause_menu.py
+++ ui/pause_menu.py
@@ -2,7 +2,7 @@ from ui.ui_base import ui_base
 from panda3d.core import TransparencyAttrib, TextNode
 from constants.events import EVENT_NAMES
 
-from direct.gui.DirectGui import DirectButton, DirectLabel, DirectFrame, DGG
+from direct.gui.DirectGui import DirectButton, DirectLabel, DirectDialog, DirectFrame, DGG
 
 class pause_menu(ui_base):
     def __init__(self):
@@ -19,10 +19,65 @@ class pause_menu(ui_base):
             loader.loadTexture("assets/textures/button_bg.png")
         )
 
+        controls_button_image = loader.loadTexture("assets/textures/controls_button.png")
+        close_button_image = loader.loadTexture("assets/textures/close_button.png")
+
         self.font = loader.loadFont("assets/fonts/NewAmsterdam-Regular.ttf")
 
         self.ui_elements = []
         self.menu_elements = []
+
+        self.controls_tutorial = DirectDialog(frameSize = (-1.2, 1.2, -0.7, 0.7),
+                                   fadeScreen = 0.4,
+                                   relief = DGG.FLAT,
+                                   frameTexture = "assets/textures/controls_tutorial.png")
+        self.controls_tutorial.setTransparency(TransparencyAttrib.MAlpha)
+        self.ui_elements.append(self.controls_tutorial)
+        self.controls_tutorial.hide()
+
+        controls_close_button = DirectButton(
+            pos=(-0.5,0,0.8), 
+            scale=0.12, 
+            command=self.controls_tutorial.hide, 
+            relief=DGG.FLAT, 
+            frameTexture = close_button_image,
+            frameSize = (-1, 1, -1, 1),
+        )
+        controls_close_button.setTransparency(TransparencyAttrib.MAlpha)
+        controls_close_button.hide()
+        self.ui_elements.append(controls_close_button)
+
+        controls_button = DirectButton(
+            pos=(-1.2,0,0.8), 
+            scale=0.12, 
+            command=self.controls_tutorial.show, 
+            relief=DGG.FLAT, 
+            text_fg=(TEXT_COLOR),
+            text_font = self.font, 
+            #text_align = TextNode.ACenter, 
+            frameTexture = controls_button_image,
+            #pad = (1, 0.1),
+            frameSize = (-1, 1, -1, 1),
+        )
+        controls_button.setTransparency(TransparencyAttrib.MAlpha)
+        self.ui_elements.append(controls_button)
+
+        
+
+        dishes_button = DirectButton(
+            pos=(-1.5,0,0.8), 
+            scale=0.12, 
+            command=self.unpause_game, 
+            relief=DGG.FLAT, 
+            text_fg=(TEXT_COLOR),
+            text_font = self.font, 
+            #text_align = TextNode.ACenter, 
+            frameTexture = controls_button_image,
+            #pad = (1, 0.1),
+            frameSize = (-1, 1, -1, 1),
+        )
+        dishes_button.setTransparency(TransparencyAttrib.MAlpha)
+        self.ui_elements.append(dishes_button)
 
         menu_box = DirectFrame(
             frameColor=TEXT_BOX_COLOR, 
