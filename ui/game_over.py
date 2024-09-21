fa46d9d637258c6ce2b6448aa4a237aa4d02b4bb
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2024-09-22 00:14:58 +0200

    add game end screen

--- ui/game_over.py
+++ ui/game_over.py
@@ -5,8 +5,10 @@ from constants.events import EVENT_NAMES
 from direct.gui.DirectGui import DirectButton, DirectLabel, DirectFrame, DGG
 
 class game_over(ui_base):
-    def __init__(self):
+    def __init__(self, score_caused):
         ui_base.__init__(self)
+
+        self.font = loader.loadFont("assets/fonts/NewAmsterdam-Regular.ttf")
 
         self.ui_elements = []
         TEXT_COLOR = (1, 0, 0, 1)  # Red color for text
@@ -22,16 +24,27 @@ class game_over(ui_base):
             relief=None  # No background box
         )
 
-        # Smaller "Your cover has been blown" text
-        self.blow_cover_label = DirectLabel(
-            text="Your cover has been blown",
-            scale=0.07,  # Smaller size
-            pos=(0, 0, 0),  # Slightly below the GAME OVER text
-            text_fg=TEXT_COLOR,  # Same red color
-            text_align=TextNode.ACenter,
-             # Use custom font if available
-            relief=None
-        )
+        if score_caused:
+            self.blow_cover_label = DirectLabel(
+                text="They worked better than you (first to reach score of 250)",
+                scale=0.07,  # Smaller size
+                pos=(0, 0, 0),  # Slightly below the GAME OVER text
+                text_fg=TEXT_COLOR,  # Same red color
+                text_align=TextNode.ACenter,
+                # Use custom font if available
+                relief=None
+            )
+        else:
+            # Smaller "Your cover has been blown" text
+            self.blow_cover_label = DirectLabel(
+                text="Your cover has been blown",
+                scale=0.07,  # Smaller size
+                pos=(0, 0, 0),  # Slightly below the GAME OVER text
+                text_fg=TEXT_COLOR,  # Same red color
+                text_align=TextNode.ACenter,
+                # Use custom font if available
+                relief=None
+            )
 
         # Set transparency (optional, in case you need transparent UI elements)
         self.game_over_label.setTransparency(TransparencyAttrib.MAlpha)
